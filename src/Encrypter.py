e1c95a52729a50e0c30f58bf81215b494f1b2cb3
Harshit Raheja <quantumbytestudios@gmail.com>
2022-05-15 14:26:18 +0530

    Update

--- src/Encrypter.py
+++ src/Encrypter.py
@@ -15,6 +15,36 @@ f.truncate(0)
 f = open("tempGenO.txt", "a")
 f.truncate(0)
 
+RGB_White = (255, 255, 255)
+NUL = (0, 0, 0)      # BLACK
+A = (153, 102, 204)  # AMETHYST
+B = (0, 0, 255)      # BLUE
+C = (0, 255, 255)    # CYAN
+D = (101, 67, 33)    # DARK BROWN
+E = (194, 178, 128)  # EBONY
+F = (181, 51, 137)   # FANDANGO
+G = (0, 171, 102)    # GREEN
+H = (170, 152, 169)  # HELIO(TROPE 204 102 153
+I = (113, 166, 210)  # ICEBERG
+J = (248, 222, 126)  # JASMINE
+K = (195, 176, 145)  # KHAKI
+L = (214, 202, 221)  # LAVENDER
+M = (255, 0, 255)    # MAGENTA
+N = (233, 255, 219)  # NYANZA
+O = (0, 128, 0)      # OCHRE
+P = (254, 40, 162)   # PINK
+Q = (142, 58, 89)    # QUINACRIDONE
+R = (183, 65, 14)    # RUST RED
+S = (244, 196, 48)   # SAFFRON
+T = (216, 191, 216)  # THISTLE
+U = (136, 120, 195)  # UBE
+V = (64, 130, 109)   # VIRIDIAN
+W = (0, 66, 66)      # WARM BLACK
+X = (115, 134, 120)  # XANADU
+Y = (255, 240, 0)    # YELLOW ROSE
+Z = (57, 167, 142)   # ZOMP
+SPACE = (222, 222, 222)
+
 STRING = input("Enter Your Message: ")
 STRING = STRING.upper()
 
